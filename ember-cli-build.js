b2438e018d2b1da6e2fedda538fffb9d41424cbb
im-komo <pravesh.technolabs@gmail.com>
2017-04-20 18:41:01 +0530

    add materialize css to project

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -20,5 +20,9 @@ module.exports = function(defaults) {
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
 
+  app.import('bower_components/materialize/dist/js/materialize.js');
+  app.import('bower_components/materialize/dist/css/materialize.css');  
+  
+
   return app.toTree();
 };
